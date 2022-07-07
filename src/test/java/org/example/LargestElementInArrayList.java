0c7bf78e4a4f18e46e3c02f0f7e4024ea8e70d46
Gayathri <v.gayathri234@gmail.com>
2022-07-07 14:29:12 -0500

    Basic Programs

---
+++ src/test/java/org/example/LargestElementInArrayList.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class LargestElementInArrayList {
+    public static void main(String[] args) {
+
+            int[] a = {2, 4, 5, 6, 7, 8};
+            int max = a[0];
+            for (int i = 0; i < a.length; i++)
+                if (a[i]>max)
+                max = a[i];
+                System.out.println(max);
+
+
+        }
+    }
--- src/test/java/org/example/SingleCharOccurance.java
+++ src/test/java/org/example/SingleCharOccurance.java
@@ -1,11 +1,11 @@
 package org.example;
 
-public class SingleCharacOccurance {
+public class SingleCharOccurance {
     public static void main(String[] args) {
         String s= "I like coding java and automation alot";
         int count = s.length();
         int replace = s.replace("i","").length();
-        int total_occurance=(count - replace);
-        System.out.println(total_occurance);
+        int total_Occurance=(count - replace);
+        System.out.println(total_Occurance);
     }
 }
